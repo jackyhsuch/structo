f199042290577aaaa07a4a0df2f145a8fe354cc3
Jacky <jackyhsu0102@gmail.com>
2017-09-27 23:37:22 +0800

    update ui and add functionalities

--- src/index.js
+++ src/index.js
@@ -1,18 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import './index.css';
 
 class ListApp extends React.Component {
     constructor() {
         super()
         this.state = {
             countries: [
-                'Singapore',
-                'Taiwan',
-                'Malaysia'
+                'Afghanistan',
+                'Australia',
+                'Bangladesh',
+                'Brazil',
+                'Brunei',
+                'Cambodia',
+                'China',
+                'Cuba',
+                'Denmark',
+                'Egypt',
+                'Finland',
+                'France',
+                'Greece',
+                'Hong Kong',
+                'Singapore'
             ],
             filteredList: [],
-            selectedList: []
+            selectedList: [],
+            active: false,
+            typing: false
         }
 
     }
@@ -23,9 +37,11 @@ class ListApp extends React.Component {
 
     filterList(e) {
         var list = this.state.countries
-        var updatedList = list.filter(country => country.toLowerCase().search(e.target.value.toLowerCase()) !== -1)
+        var updatedList = list.filter(country => country.toLowerCase().startsWith(e.target.value.toLowerCase()))
  
         this.setState({filteredList: updatedList})
+        this.setState({typing: (e.target.value !== '') ? true:false})
+
     }
 
     handleClick(e) {
@@ -40,9 +56,11 @@ class ListApp extends React.Component {
         // remove selected from filtered and overall lists
         var list = this.state.countries
         var updatedList = list.filter(country => updatedselectedList.indexOf(country) === -1)
+        var filteredList = this.state.filteredList
+        var updatedFilteredList = filteredList.filter(country => updatedselectedList.indexOf(country) === -1)
         this.setState({
             countries: updatedList,
-            filteredList: updatedList
+            filteredList: updatedFilteredList
         })
     }
 
@@ -51,55 +69,113 @@ class ListApp extends React.Component {
             if (e.target.value !== '') {
                 var updatedFilteredList = this.state.filteredList
                 var updatedselectedList = this.state.selectedList
+                if (updatedFilteredList.length > 0) {
+                    // add selected to selectedList
+                    var selectedCountry = updatedFilteredList[0]
+                    updatedselectedList.push(selectedCountry)
+                    this.setState({selectedList: updatedselectedList})
 
-                // add selected to selectedList
-                var selectedCountry = updatedFilteredList[0]
-                updatedselectedList.push(selectedCountry)
-                this.setState({selectedList: updatedselectedList})
+                    // remove selected from filteredList
+                    this.setState({filteredList: updatedFilteredList.slice(1,)})
 
-                // remove selected from filteredList
-                this.setState({filteredList: updatedFilteredList.slice(1,)})
-
-                // remove selected from overall list
-                var list = this.state.countries
-                var updatedList = list.filter(country => updatedselectedList.indexOf(country) === -1)
-                this.setState({countries: updatedList})
+                    // remove selected from overall list
+                    var list = this.state.countries
+                    var updatedList = list.filter(country => updatedselectedList.indexOf(country) === -1)
+                    this.setState({countries: updatedList})
+                }
             }
         }
+    }
+
+    handleRemove(e) {
+        var updatedselectedList = this.state.selectedList
+        var selectedCountry = e.target.dataset.country
+
+        // remove selected from selectedList
+        var index = updatedselectedList.indexOf(selectedCountry)
+        updatedselectedList.splice(index, 1)
+
+        this.setState({selectedList: updatedselectedList})
+
+        // add back selected to filtered and overall lists
+        var list = this.state.countries
+        var updatedList = list
+        updatedList.push(selectedCountry)
+        updatedList.sort()
+        this.setState({
+            countries: updatedList,
+            filteredList: updatedList
+        })
+    }
+
+    toggleAcitve(e) {
+        var updatedActive = !this.state.active
+        this.setState({active: updatedActive})
+    }
+
+    setDropdownClass(isShow, isTyping) {
+        var hidden = (!isShow) ? "hidden":""
+        var typing = (isTyping) ? "typing":""
+        var classes = hidden + " " + typing + " dropdown"
+
+        return classes
+
+    }
+
+    setDropdownButtonClass(isShow) {
+        if (isShow) {
+            return "glyphicon glyphicon-triangle-top"
+        }
+
+        return "glyphicon glyphicon-triangle-bottom"
     }
 
     render() {
         return (
             <div className="list-app">
-                <div>
-                {
-                    this.state.selectedList.map(function(country) {
-                        return (
-                            <div 
-                                key={country}
-                                data-country={country}
-                            >{country}</div>
-                        )
-                    })
-                }
+                <div className="input-area" onClick={()=>this.textInput.focus()}>
+                    {
+                        this.state.selectedList.map(function(country) {
+                            return (
+                                <div 
+                                    key={country}
+                                    className="selected-tags"
+                                    data-country={country}>
+                                {country} <span 
+                                            className="glyphicon glyphicon-remove tag-remove" 
+                                            aria-hidden="true"
+                                            data-country={country}
+                                            onClick={e=>this.handleRemove(e)}></span>
+                                </div>
+                            )
+                        }, this)
+                    }
+                    <input 
+                        className="input"
+                        type="text" 
+                        onChange={e=>this.filterList(e)}
+                        onKeyUp={e=>this.handleEnter(e)}
+                        ref={input=>this.textInput = input}/>
+
+                    <label className="input-btn" onClick={e=>this.toggleAcitve(e)}>
+                    <span className={this.setDropdownButtonClass(this.state.active)} aria-hidden="true"></span>
+                    </label>
                 </div>
-                <input type="text" 
-                    onChange={e => this.filterList(e)}
-                    onKeyUp={e => this.handleEnter(e)}
-                />
-                <div>
+                <div className={this.setDropdownClass(this.state.active, this.state.typing)}>
                 {
                     this.state.filteredList.map(function(country) {
                         return (
                             <div 
                                 key={country}
                                 data-country={country}
-                                onClick={e => this.handleClick(e)}
-                            >{country}</div>
+                                onClick={e=>this.handleClick(e)}>
+                            {country}
+                            </div>
                         )
                     }, this)
                 }
                 </div>
+                <div className="clearfix"></div>
             </div>
         )
     }
